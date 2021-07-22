b7c06d5a3ec68ad9b80e8999859ab2636c3ac79a
Devin Pile <devinpile@gmail.com>
2021-07-22 15:18:09 -0600

    Add model a

---
+++ models/model_a.rb
@@ -0,0 +1,1 @@
+School = {Name: "Jefferson High School", State: "NY", Est: 1986, number_of_students: 732, is_open: true, is_enrolling: true, grades:[9,10,11,12], school_colors: ["black","red","white"]}
