faf2a1ec99977953c69ef57f4de1381343648b17
Devin Pile <devinpile@gmail.com>
2021-07-22 15:24:21 -0600

    Add model b

---
+++ models/model_b.rb
@@ -0,0 +1,1 @@
+Pet = {name: "Beckett", breed: "Bengal", age:6 , weight_in_lbs: 7.5, is_good_kitty: true, is_happy_kitty: true, favorite_activity: ["Sleep","Eat","Cuddle"], vocab: ["meow","hiss","purr"]}
