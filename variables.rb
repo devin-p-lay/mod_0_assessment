166a44d7e69269afb6b26529aabe2cecf0d4d50b
Devin Pile <devinpile@gmail.com>
2021-07-22 15:08:37 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Olivia Rodrigo"
+is_active = true
+famous_person = "Giannis Antetoukounmpo"
+num_3 = 50
+num_9 = 69
+sum = num_3 + num_9
+animals = ["lions","tigers","bears"]
+animals.shift
+p famous_person.length
